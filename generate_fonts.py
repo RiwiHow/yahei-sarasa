43c954cc7b2623fb069e118a594d63031ed5d73e
RiwiHow <53964574+RiwiHow@users.noreply.github.com>
2024-11-12 13:25:21 +0800

    refactor: only modify ui fonts since SegoeUI only has ui fonts

--- generate_fonts.py
+++ generate_fonts.py
@@ -19,24 +19,6 @@ def get_version(font):
     return font.version.split(';')[0]
 
 
-def set_regular_names(font):
-    font.fontname = 'MicrosoftYaHei'
-    font.familyname = 'Microsoft YaHei'
-    font.fullname = 'Microsoft YaHei'
-    font.version = get_version(font)
-    font.copyright = conf.COPYRIGHT
-    font.sfnt_names = (
-        ('English (US)', 'Family', 'Microsoft YaHei'),
-        ('English (US)', 'Fullname', 'Microsoft YaHei'),
-        ('English (US)', 'UniqueID', 'Microsoft YaHei'),
-        ('English (US)', 'SubFamily', 'Regular'),
-        ('English (US)', 'Version', get_version(font)),
-        ('English (US)', 'Copyright', conf.COPYRIGHT),
-        ('Chinese (PRC)', 'Family', '微软雅黑'),
-        ('Chinese (PRC)', 'Fullname', '微软雅黑')
-    )
-
-
 def set_regular_ui_names(font):
     font.fontname = 'MicrosoftYaHeiUI'
     font.familyname = 'Microsoft YaHei UI'
@@ -55,24 +37,6 @@ def set_regular_ui_names(font):
     )
 
 
-def set_bold_names(font):
-    font.fontname = 'MicrosoftYaHeiBold'
-    font.familyname = 'Microsoft YaHei'
-    font.fullname = 'Microsoft YaHei Bold'
-    font.version = get_version(font)
-    font.copyright = conf.COPYRIGHT
-    font.sfnt_names = (
-        ('English (US)', 'Family', 'Microsoft YaHei'),
-        ('English (US)', 'Fullname', 'Microsoft YaHei Bold'),
-        ('English (US)', 'UniqueID', 'Microsoft YaHei Bold'),
-        ('English (US)', 'SubFamily', 'Bold'),
-        ('English (US)', 'Version', get_version(font)),
-        ('English (US)', 'Copyright', conf.COPYRIGHT),
-        ('Chinese (PRC)', 'Family', '微软雅黑'),
-        ('Chinese (PRC)', 'Fullname', '微软雅黑 Bold')
-    )
-
-
 def set_bold_ui_names(font):
     font.fontname = 'MicrosoftYaHeiUIBold'
     font.familyname = 'Microsoft YaHei UI'
@@ -88,24 +52,6 @@ def set_bold_ui_names(font):
         ('English (US)', 'Copyright', conf.COPYRIGHT),
         ('Chinese (PRC)', 'Family', '微软雅黑 UI'),
         ('Chinese (PRC)', 'Fullname', '微软雅黑 UI Bold')
-    )
-
-
-def set_light_names(font):
-    font.fontname = 'MicrosoftYaHeiLight'
-    font.familyname = 'Microsoft YaHei'
-    font.fullname = 'Microsoft YaHei Light'
-    font.version = get_version(font)
-    font.copyright = conf.COPYRIGHT
-    font.sfnt_names = (
-        ('English (US)', 'Family', 'Microsoft YaHei'),
-        ('English (US)', 'Fullname', 'Microsoft YaHei Light'),
-        ('English (US)', 'UniqueID', 'Microsoft YaHei Light'),
-        ('English (US)', 'SubFamily', 'Light'),
-        ('English (US)', 'Version', get_version(font)),
-        ('English (US)', 'Copyright', conf.COPYRIGHT),
-        ('Chinese (PRC)', 'Family', '微软雅黑'),
-        ('Chinese (PRC)', 'Fullname', '微软雅黑 Light')
     )
 
 
@@ -128,56 +74,27 @@ def set_light_ui_names(font):
 
 
 def gen_regular():
-    fs.copy(conf.TEMP_DIR + '/' + conf.REGULAR_SOURCE + '.ttf',
-            conf.TEMP_DIR + '/' + conf.REGULAR_SOURCE + '-UI.ttf')
-
-    font = open_font(conf.TEMP_DIR + '/' + conf.REGULAR_SOURCE + '.ttf')
-    remove_gasp(font)
-    set_cleartype(font)
-    set_regular_names(font)
-
-    font_ui = open_font(conf.TEMP_DIR + '/' +
-                        conf.REGULAR_SOURCE + '-UI.ttf')
+    font_ui = open_font(conf.TEMP_DIR + '/' + conf.REGULAR_SOURCE + '.ttf')
     remove_gasp(font_ui)
     set_cleartype(font_ui)
     set_regular_ui_names(font_ui)
 
-    font.generateTtc(conf.TEMP_DIR + '/msyh.ttc',
-                     font_ui, ttcflags=('merge'), layer=1)
+    font_ui.generateTtc(conf.TEMP_DIR + '/segoeui.ttf')
 
 
 def gen_bold():
-    fs.copy(conf.TEMP_DIR + '/' + conf.BOLD_SOURCE + '.ttf',
-            conf.TEMP_DIR + '/' + conf.BOLD_SOURCE + '-UI.ttf')
-
-    font = open_font(conf.TEMP_DIR + '/' + conf.BOLD_SOURCE + '.ttf')
-    remove_gasp(font)
-    set_cleartype(font)
-    set_bold_names(font)
-
-    font_ui = open_font(conf.TEMP_DIR + '/' + conf.BOLD_SOURCE + '-UI.ttf')
+    font_ui = open_font(conf.TEMP_DIR + '/' + conf.BOLD_SOURCE + '.ttf')
     remove_gasp(font_ui)
     set_cleartype(font_ui)
     set_bold_ui_names(font_ui)
 
-    font.generateTtc(conf.TEMP_DIR + '/msyhbd.ttc',
-                     font_ui, ttcflags=('merge'), layer=1)
+    font_ui.generateTtc(conf.TEMP_DIR + '/segoeuib.ttf')
 
 
 def gen_light():
-    fs.copy(conf.TEMP_DIR + '/' + conf.LIGHT_SOURCE + '.ttf',
-            conf.TEMP_DIR + '/' + conf.LIGHT_SOURCE + '-UI.ttf')
-
-    font = open_font(conf.TEMP_DIR + '/' + conf.LIGHT_SOURCE + '.ttf')
-    remove_gasp(font)
-    set_cleartype(font)
-    set_light_names(font)
-
-    font_ui = open_font(conf.TEMP_DIR + '/' +
-                        conf.LIGHT_SOURCE + '-UI.ttf')
+    font_ui = open_font(conf.TEMP_DIR + '/' + conf.LIGHT_SOURCE + '.ttf')
     remove_gasp(font_ui)
     set_cleartype(font_ui)
     set_light_ui_names(font_ui)
 
-    font.generateTtc(conf.TEMP_DIR + '/msyhl.ttc',
-                     font_ui, ttcflags=('merge'), layer=1)
+    font_ui.generateTtc(conf.TEMP_DIR + '/segoeuil.ttf')
